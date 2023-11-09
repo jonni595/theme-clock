47e469f62cc148a8daf3d9f1aef612001322a76f
Jonathan <jonathandajome@gmail.com>
2023-11-09 15:35:43 -0500

    add dates file

---
+++ src/utils/calendar.ts
@@ -0,0 +1,23 @@
+export const days: string[] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+export const months: string[] = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
