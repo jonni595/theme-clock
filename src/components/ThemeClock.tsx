7fea3f2ef4c506e623acbf6f674615577367836b
Jonathan <jonathandajome@gmail.com>
2023-11-09 22:57:43 -0500

    refactor theme clock component

--- src/components/ThemeClock.tsx
+++ src/components/ThemeClock.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { days, months } from "../utils/calendar";
+import Clock from "./Clock";
 
 const ThemeClock = () => {
   const [time, setTime] = useState<Date>(new Date());
@@ -13,38 +15,24 @@ const ThemeClock = () => {
     };
   }, []);
 
-  const scale = (
-    num: number,
-    in_min: number,
-    in_max: number,
-    out_min: number,
-    out_max: number
-  ) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-  };
-
   const hoursForClock =
     time.getHours() >= 13 ? time.getHours() % 12 : time.getHours();
-  const ampm = time.getHours() >= 12 ? "PM" : "AM";
+  const formatAmPm = time.getHours() >= 12 ? "PM" : "AM";
+  const minutesForClock =
+    time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+
+  const date = `${days[time.getDay()]}, ${months[time.getMonth()]}`;
 
   return (
-    <>
-      <button className="toggle"></button>
-      <main className="container">
-        <div className="container__clock">
-          <div className="needle hour"></div>
-          <div className="needle minute"></div>
-          <div className="needle second"></div>
-          <div className="center-point"></div>
-        </div>
-        <div className="container__time">{`${hoursForClock}:${
-          time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
-        } ${ampm}`}</div>
-        <div className="container__date">{`${days[time.getDay()]}, ${
-          months[time.getMonth()]
-        } <span class="circle">${time.getDate()}</span>`}</div>{" "}
-      </main>
-    </>
+    <main className="container">
+      <button className="toggle">Button</button>
+      <Clock
+        hour={`${hoursForClock}:${minutesForClock}`}
+        format={formatAmPm}
+        date={date}
+        day={time.getDate()}
+      />
+    </main>
   );
 };
 
