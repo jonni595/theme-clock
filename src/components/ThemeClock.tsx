ef2edd4049d7351bba3875627d57b86bbbcaff16
Jonathan <jonathandajome@gmail.com>
2023-11-10 01:09:45 -0500

    optimize theme clock component

--- src/components/ThemeClock.tsx
+++ src/components/ThemeClock.tsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
 import { days, months } from "../utils/calendar";
 import Clock from "./Clock";
+import { ThemeContext } from "../utils/ThemeContext";
 
 const ThemeClock = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
   const [time, setTime] = useState<Date>(new Date());
 
   useEffect(() => {
@@ -15,19 +18,32 @@ const ThemeClock = () => {
     };
   }, []);
 
-  const hoursForClock =
-    time.getHours() >= 13 ? time.getHours() % 12 : time.getHours();
+  const hoursForClock = time.getHours() % 12 || 12;
+  const formattedHours = hoursForClock.toString().padStart(2, "0");
+
   const formatAmPm = time.getHours() >= 12 ? "PM" : "AM";
   const minutesForClock =
     time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
 
   const date = `${days[time.getDay()]}, ${months[time.getMonth()]}`;
 
+  const buttonClass = theme === "light" ? "dark" : "light";
+  const buttonText = theme === "light" ? "dark mode" : "light mode";
+  const buttonIcon =
+    theme === "light" ? (
+      <BsMoonFill className="icon" />
+    ) : (
+      <BsSunFill className="icon" />
+    );
+
   return (
-    <main className="container">
-      <button className="toggle">Button</button>
+    <main className={`container ${theme}`}>
+      <button className={`toggle ${buttonClass}`} onClick={toggleTheme}>
+        {buttonText}
+        {buttonIcon}
+      </button>
       <Clock
-        hour={`${hoursForClock}:${minutesForClock}`}
+        hour={`${formattedHours}:${minutesForClock}`}
         format={formatAmPm}
         date={date}
         day={time.getDate()}
