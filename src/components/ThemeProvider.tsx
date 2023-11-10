af6a5a16e02facb1063b0b858894a004103c9efd
Jonathan <jonathandajome@gmail.com>
2023-11-09 23:33:53 -0500

    create provider

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { ThemeContext } from "../utils/ThemeContext";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
